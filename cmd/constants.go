8181d45a16367baafdd6ec649efea5635f31ab91
John Halbert <john.halbert@mariadb.com>
2021-09-21 14:57:38 +0000

    Introduce constants, refactor to use them in root

---
+++ cmd/constants.go
@@ -0,0 +1,20 @@
+package cmd
+
+// Identifiers
+const (
+	CLI_NAME     = "skysqlcli"
+	PROJECT_NAME = "skysql"
+)
+
+// Actions
+const (
+	GET    = "get"
+	CREATE = "create"
+	DELETE = "delete"
+	UPDATE = "update"
+)
+
+// Entities
+const (
+	DATABASE = "database"
+)
